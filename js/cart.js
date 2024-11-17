9b335cf11d29b6a291cae7a7e60110abbfe3dc5c
Bohdana <berenka.bog@gmail.com>
2024-11-17 20:21:02 +0200

    Bookshop

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,7 @@
 let orderBtn = document.querySelector("#orderBtn");
+let orderSection = document.querySelector(".order");
+
+orderBtn.addEventListener("click", function (){
+    orderBtn.computedStyleMap.display = "none";
+    orderSection.computedStyleMap.display = "block";
+});
