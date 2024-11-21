1b9f6955d0629f00d12088c3cd2b5bcfd30c57ce
Bohdana <berenka.bog@gmail.com>
2024-11-21 17:22:23 +0200

    Bookshop

--- js/script.js
+++ js/script.js
@@ -120,3 +120,16 @@ async function getProducts() {
   
   // Створення об'єкта кошика
   let cart = new ShoppingCart();
+
+// динамічна сторінка
+
+  document.addEventListener("DOMContentLoaded", function () {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach(function (card) {
+    card.addEventListener("click", function () {
+      const cardId = card.getAttribute("data-id");
+      window.location.href = `details.html?id=${cardId}`;
+    });
+  });
+});
